178307151ec61cea59b99e475b3a6c7469692237
vllddlvh <147725837+vllddlvh@users.noreply.github.com>
2025-04-01 19:11:34 +0700

    config springboot

---
+++ API Cinema Web/API-Cinema-Web/src/main/java/com/cinemaweb/API/Cinema/Web/ApiCinemaWebApplication.java
@@ -0,0 +1,13 @@
+package com.cinemaweb.API.Cinema.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCinemaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCinemaWebApplication.class, args);
+	}
+
+}
