621a900e4ebfc86f3077ceadb5fc6f79444849ae
PhuongBui69 <buihaiphupng6@gmail.com>
2025-04-29 16:34:41 +0700

    refactor

--- API Cinema Web/API-Cinema-Web/src/main/java/com/cinemaweb/API/Cinema/Web/scheduler/ScheduledTasks.java
+++ API Cinema Web/API-Cinema-Web/src/main/java/com/cinemaweb/API/Cinema/Web/scheduler/ScheduledTasks.java
@@ -7,7 +7,10 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -18,6 +21,9 @@ public class ScheduledTasks {
     InvalidatedTokenRepository invalidatedTokenRepository;
     PasswordOtpRepository passwordOtpRepository;
 
-    
-
+    @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
+    public void invalidatedTokenCleaning() {
+        invalidatedTokenRepository.deleteAllByExpiryTime();
+        log.info("Scheduled task: CLEANING_INVALIDATED_TOKEN");
+    }
 }
